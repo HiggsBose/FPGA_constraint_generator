3d5f4eec89d72769904b84c85f1cc1334ae3b9fc
HiggsBose <72658195+HiggsBose@users.noreply.github.com>
2024-09-09 16:46:18 +0800

    Update Constraint_Generator.py
    Add notes for versatility

--- Constraint_Generator.py
+++ Constraint_Generator.py
@@ -11,14 +11,14 @@ def excel_generator(address, sheet_name):
     """
     df = pd.read_excel(address, sheet_name=sheet_name)
 
-    signal_name = "Pin Name"
-    pin_type_name = "类型"
-    fpga_pin_name = "FPGA引脚"
+    signal_name = "PIN Name"    # the name of the column of signal names in FPGA
+    pin_type_name = "Type"      # Pin type definition, IN or OUT
+    fpga_pin_name = "FPGA PIN"  # the name of the column of FPGA PIN names
 
     assert len(df[signal_name]) == len(df[fpga_pin_name])
 
     for index in range(len(df[signal_name])):
-        if df[pin_type_name][index] != "S_4_IN" and df[pin_type_name][index] != "S_4_OUT":
+        if df[pin_type_name][index] != "IN" and df[pin_type_name][index] != "OUT":
             continue
 
         df[signal_name][index] = df[signal_name][index].replace("<", "[")
@@ -57,6 +57,6 @@ def csv_generator(file_name):
         file.close()
 
 
-file_address = "D:/ClassDocuments/Research/设计/流片/22_12_CHIP/Test/4T2R/signal_mapping.xlsx"
-sheet_name = "访问阵列"
+file_address = "file_address" # Please enter the address of the .xlsx or .csv file here
+sheet_name = "sheet_name" # enter the sheet name
 excel_generator(file_address, sheet_name)
